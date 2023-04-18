26fa8867a52d6be47b1f1b124aa4ad771157cf72
Kartoffel-co <pablo.aguilareal@gmail.com>
2023-04-18 13:56:05 +0200

    ScriptsConBBDD

---
+++ PabloViñolo_ScriptsConBBDD.sql
@@ -0,0 +1,32 @@
+
+--NBA
+--1 Con la bbdd de la NBA imprime por consola el numero de partidos que ha jugado Ricky Rubio. Guardalo en una variable y luego imprimelo con el print.
+
+--2 Recupera el numero de partidos de Ricky Rubio y el de Lebron James en dos variables. Imprime por consola el nombre del jugador que más partidos ha disputado.
+
+--3 Recupera en una variable la fecha del ultimo partido de la nba que tenemos en la bbdd. Muestra esa fecha por consola.
+
+--4 Recupera el nombre, la altura y el peso de de Pau Gasol. Imprime un mensaje que ponga "Pau Gasol mide XX y pesa XX. Su indice de masa corporal es: " ( busca como calcular el IMC en google.)
+-- Para poder convertir un decimal o un entero a varchar, podemos usar CAST(@altura as varchar(100)) o directamente declarar la variable altura como un varchar(100).
+
+use arepazo
+
+--arepazo
+--5 Añade 1000 veces un clientes los mismos datos de contacto.
+
+--6 Borra esos clientes duplicados, porfavor.
+
+--7 Recupera el cod postal, el telefono , el email y el nombre del cliente que más dinero se ha gastado en 4 variables distintas.
+--muestra en la consola los valores de las 4 variables
+
+--8 Recupera el valor de total del ultimo pedido de el cliente id =1. Si el total es impar, cambia los gastos de envio de ese pedido a 0. 
+--Si el total es par, duplica los gastos de envio. Recalcula luego el total de ese pedido.
+
+--9 Sacad el numero total de elementos del menu que son bebidas del arepazo 
+--por consola
+
+--10 Calcular el cliente que mas ha gastado en 2020 en el arepazo, y mandarle a su casa un pedido de 5 arepas arepazo
+--a coste 0.
+
+--11 Crear un script que te indique si el menu es rentable o no dependiendo de si hay elementos en el menu no rentables.
+--debe devolver 'ES RENTABLE' o '¡ESTAMOS PERDIENDO PASTA!' por la consola.
