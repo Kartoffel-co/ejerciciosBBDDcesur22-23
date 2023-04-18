979e52cc7d5e2d25d71719b5e3d60ca1dc7ab960
Kartoffel-co <pablo.aguilareal@gmail.com>
2023-04-18 10:18:12 +0200

    t-sql terminado

--- script_Tsql.sql
+++ script_Tsql.sql
@@ -57,7 +57,13 @@ WHILE @count <> 10
     END
 
 --6.-calcular letra DNI ( (el % sirve para calcular el resto de una división (ejemplo: select 3%2 )))
+DECLARE @resto as int = 0
+DECLARE @numDNI as int = 12345678
+DECLARE @letraDNI as VARCHAR(1)
 
+SET @resto = (@numDNI % 23) + 1
+set @letraDNI = substring('TRWAGMYFPDXBNJZSQVHLCKE',@resto,1)
+PRINT cast(@numDNI as VARCHAR(9)) + @letraDNI
 
 --7 ¿Cuántos multiplos de 3 hay entre el 1 y el 1000?
 DECLARE @count as int = 1
@@ -75,9 +81,7 @@ WHILE @count <> 1000
 --8 ¿es el numero 83 un numero primo? (el % sirve para calcular el resto de una división (ejemplo: select 3%2 ))
 DECLARE @count as int = 1
 DECLARE @num as int
-DECLARE @num1 as int
-DECLARE @num2 as int
-DECLARE @num3 as int
+DECLARE @contar as int=0
 DECLARE @primo as int = 83
 
 
@@ -85,20 +89,24 @@ WHILE @count <= @primo
     BEGIN
         set @num = @primo % @count
         -- print @num
-        if @num = 0 and @count = 1
+        if @num = 0
             BEGIN
-                set @num1 = @num
-            END
-        IF @num = 0 and @count = @primo
-            BEGIN
-                set @num2 = @num
-            END
-        IF @num1 = 0 and @num2 = 0
-            BEGIN
-                PRINT 'es numero primo'
+                set @contar = @contar + 1
+               -- PRINT @contar
             END
         set @count = @count + 1
-    END  
+       
+        -- PRINT @contar
+        -- print @count
+    END
+IF @contar = 2
+    BEGIN
+        PRINT 'es numero primo'
+    END
+else
+    BEGIN
+        PRINT 'no es primo'
+    END
 
 
 --9 La sucesión de Fibonacci se define de la siguiente forma: a1=1, a2=1 y an=an-1+an-2 para n>2, 
